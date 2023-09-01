717b5f369137bc998cfc3813ffdff145be87edf7
brunoCarvalhoo <brunocdnr@gmail.com>
2023-09-01 17:08:13 -0300

    primeiroCommit

---
+++ calculadora.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Função de Soma
+int soma(int a, int b){
+    int result = a + b;
+    return result;
+}
+
+// Função Subtração
+int subtracao(int a, int b){
+    int result = a - b;
+    return result;
+}
+
---
+++ calculadora.h
@@ -0,0 +1,8 @@
+#ifndef H_CALCULADORA
+#define H_CALCULADORA
+
+int soma(int a, int b);
+int subtracao(int a, int b);
+
+
+#endif
